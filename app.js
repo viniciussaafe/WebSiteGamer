b1e5b6afcdafaba17ae99e94d1cf5d5a2772f83d
Vinicius Saafe <148727281+viniciussaafe@users.noreply.github.com>
2025-03-31 15:12:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const nextButton = document.querySelector('.next-btn');
 const video = document.querySelector('.hero-video');
 
-const movieList = ['videos/hero-1.mp4', 'videos/hero-2.mp4', 'videos/hero-3.mp4', 'videos/hero-4.mp4'];
+const movieList = ['videos/hero-1.mp4', 'videos/hero-2.mp4'];
 
 let index = 0;
 nextButton.addEventListener('click', function () {
